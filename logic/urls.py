98107bef0b024ea129a3e13cc17c4d472eb86ec4
Jaffar Hussein <jaffhussein@icloud.com>
2022-06-05 18:19:22 +0200

    ft:Login- Created the login logic

--- logic/urls.py
+++ logic/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path("register", views.register_request, name="register")
+    path('',views.home,name='home'),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
 ]
--- logic/views.py
+++ logic/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render, redirect
-from .forms import NewUserForm
+from .forms import NewUserForm,LoginForm
 # Create your views here.
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate
 from django.contrib import messages
+
+
 def home(request):
-    return render(request,'layout.html')
+    return render(request, 'layout.html')
+
+
 def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("home")
+        messages.error(
+            request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request, template_name="auth/register.html", context={"register_form": form})
+
+
+def login_request(request):
+	form = LoginForm()
+	context = {
+		"form": form,
+	}
 	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
 			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("home")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="auth/register.html", context={"register_form":form})
+			
+			messages.success(request, "Successfully logged in")
+			return redirect ('home')
+		else:
+			messages.error(request, "Invalid username or password")
+			return redirect ('login')
+		
+	context = {
+		"form": form,
+	}
+	return render(request, 'auth/login.html', context=context)
