2d03c8069641d41fc4303ef212eef375db903e52
Jaffar Hussein <jaffhussein@icloud.com>
2022-06-05 20:23:35 +0200

    ft:Login completion

--- logic/forms.py
+++ logic/forms.py
@@ -3,24 +3,31 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class NewUserForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True)
 
-	class Meta:
-		model = User
-		fields = ("username", "email", "password1", "password2")
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
 
-	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
-		user.email = self.cleaned_data['email']
-		if commit:
-			user.save()
-		return user
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
 
 class LoginForm(forms.ModelForm):
-    username = forms.TextInput()
+    username = forms.CharField(max_length=80)
     password = forms.CharField(widget=forms.PasswordInput())
-    
+    # required_css_class = 'required d-none'
+    username.widget.attrs.update(
+        {'class': 'form-control m-2 w-75 input-val', 'placeholder': 'Username'})
+    password.widget.attrs.update(
+        {'class': 'form-control m-2 w-75 input-val', 'placeholder': 'Password'})
+
     class Meta:
         model = Profile
         fields = ('username', 'password')
