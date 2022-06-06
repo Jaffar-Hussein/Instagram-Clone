23c3f834694ffb794dbe6299e8f9adf62b838f26
Jaffar Hussein <jaffhussein@icloud.com>
2022-06-06 11:21:28 +0200

    ft:nav - created nav bar

--- logic/views.py
+++ logic/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import NewUserForm,LoginForm, ImageForm
+from .forms import NewUserForm, LoginForm, ImageForm
 # Create your views here.
-from django.contrib.auth import login,authenticate
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
 
@@ -23,47 +23,41 @@ def register_request(request):
 
 
 def login_request(request):
-	form = LoginForm()
-	context = {
-		"form": form,
-	}
-	if request.method == "POST":
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			
-			messages.success(request, "Successfully logged in")
-			return redirect ('home')
-		else:
-			messages.error(request, "Invalid username or password")
-			return redirect ('login')
-		
-	context = {
-		"form": form,
-	}
-	return render(request, 'auth/login.html', context=context)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            messages.success(request, "Successfully logged in")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect('login')
+    form = LoginForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'auth/login.html', context=context)
+
 
 def image_upload(request):
-	context = {
-		"form":form
-	}
-	form=ImageForm()
+    context = {
+        "form": form
+    }
+    form = ImageForm()
 
-	if request.method == 'POST':
-		form=ImageForm(request.POST,request.FILES)
-		if form.is_valid():
-			name= form.cleaned_data['name']
-			image= form.cleaned_data['image']
-			caption= form.cleaned_data['caption']
-			form=ImageForm(name=name,image=image,caption=caption)
-			form.save()
-			print(form)
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            image = form.cleaned_data['image']
+            caption = form.cleaned_data['caption']
+            form = ImageForm(name=name, image=image, caption=caption)
+            form.save()
+            print(form)
 
-			messages.success(request, "Post created successful")
-	
-	
+            messages.success(request, "Post created successful")
 
-	
-	return render(request, 'home.html',context=context)
+    return render(request, 'home.html', context=context)
