5274d07605b3c4b903e9021ab376ea36505e7581
Jaffar Hussein <jaffhussein@icloud.com>
2022-06-06 18:29:40 +0200

    ft:Log out and user verification

--- logic/urls.py
+++ logic/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
     path('upload', views.image_upload, name='upload'),
     path('profile', views.profile, name='profile'),
-    path('logout', views.logout, name='logout'),
+    path('logout', views.logout_request, name='logout'),
+    
 ]
--- logic/views.py
+++ logic/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm, LoginForm, ImageForm
 # Create your views here.
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Image,Profile
+from .models import Image, Profile
 
 
+@login_required
 def home(request):
     return render(request, 'home.html')
 
 
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			username= form.cleaned_data['username']
-			email= form.cleaned_data['email']
-			user = form.save()
-			profile=Profile(username=username, email=email)
-			profile.save()
-			messages.success(request, "Registration successful, Please Login")
-			return redirect("login")
-		messages.error(
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            user = form.save()
+            profile = Profile(username=username, email=email)
+            profile.save()
+            messages.success(request, "Registration successful, Please Login")
+            return redirect("login")
+        messages.error(
             request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render(request, template_name="auth/register.html", context={"register_form": form})
+    form = NewUserForm()
+    return render(request, template_name="auth/register.html", context={"register_form": form})
 
 
 def login_request(request):
@@ -47,36 +49,41 @@ def login_request(request):
     return render(request, 'auth/login.html', context=context)
 
 
+@login_required
 def image_upload(request):
 
     if request.method == 'POST':
-        form=ImageForm(request.POST,request.FILES)
+        form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
-            name= form.cleaned_data['name']
-            image= form.cleaned_data['image']
-            caption= form.cleaned_data['caption']
-            likes=0
-            profile=Profile.objects.all().filter(username=request.user.username).first()
+            name = form.cleaned_data['name']
+            image = form.cleaned_data['image']
+            caption = form.cleaned_data['caption']
+            likes = 0
+            profile = Profile.objects.all().filter(username=request.user.username).first()
             print(profile)
-            form=Image(name=name,image=image,caption=caption,likes=likes,profile=profile)
+            form = Image(name=name, image=image, caption=caption,
+                         likes=likes, profile=profile)
             form.save()
             print(form)
 
             messages.success(request, "Post created successful")
-    form=ImageForm()
+    form = ImageForm()
 
     context = {
-		"form":form
-	}
+        "form": form
+    }
 
-	
-    return render(request, 'upload.html',context=context)
+    return render(request, 'upload.html', context=context)
 
-def logout(request):
-    return render(request, '')
 
+def logout_request(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
 def profile(request):
     context = {
-        
+
     }
     return render(request, 'profile.html', context=context)
